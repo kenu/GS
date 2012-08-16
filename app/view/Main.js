0d05dd3ee40e0ed78c5d8bf07619fe2e3f9a91bd
okjspgwt <okjspgwt@gmail.com>
2012-08-16 16:21:34 +0900

    init

---
+++ app/view/Main.js
@@ -0,0 +1,19 @@
+Ext.define("GS.view.Main", {
+    extend: 'Ext.tab.Panel',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+            	xtype: 'homepanel'
+            },
+            {
+            	xtype: 'contactform'
+            }
+        ]
+    }
+});
